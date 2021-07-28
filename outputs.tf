0db9bda600cc6ce019b5e3a33756cc93edd37216
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-27 22:08:28 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,8 @@
-output "bastion_host_1_public_ip" {  
-  value = "${aws_eip.bastion_elastic_ip_1.public_ip}"
+output "bastion_host_public_ip" {  
+  value = "${aws_eip.bastion_elastic_ip.public_ip}"
 }
 
-output "bastion_host_1_private_ip" {  
-  value = "${aws_instance.bastion_host_1.private_ip}"
+output "bastion_host_private_ip" {  
+  value = "${aws_eip.bastion_elastic_ip.private_ip}"
 }
 
-output "bastion_host_2_public_ip" {  
-  value = "${aws_eip.bastion_elastic_ip_2.public_ip}"
-}
-
-output "bastion_host_2_private_ip" {  
-  value = "${aws_instance.bastion_host_2.private_ip}"
-}
