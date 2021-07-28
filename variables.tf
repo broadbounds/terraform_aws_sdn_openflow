a946504ebe9218bec138aca0ba204460cf13cc6c
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-27 22:03:33 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,31 +8,6 @@ variable "AZ_1" {
   default = "use2-az1"
 }
 
-variable "AZ_2" {
-  description = "Availability zone 2"
-  default = "use2-az2"
-}
-
-variable "ec2_type" {
-  description = "The type of ec2 instances to create"
-  default = "t2.micro"
-}
-
-variable "ec2_ami" {
-  description = "The ami image to use for ec2 instances"
-  default = "ami-077e31c4939f6a2f3"
-}
-
-variable "access_key" {
-  type        = string
-  default     = ""
-}
-
-variable "secret_key" {
-  type        = string
-  default     = ""
-}
-
 variable "vpc_cidr" {
   description = "VPC CIDR"
   default = "192.168.0.0/16"
@@ -43,19 +18,29 @@ variable "public_subnet_1_CIDR" {
   default = "192.168.1.0/24"
 }
 
-variable "public_subnet_2_CIDR" {
-  description = "Public Subnet AZ 1 CIDR"
-  default = "192.168.2.0/24"
-}
-
 variable "private_subnet_1_CIDR" {
   description = "Private Subnet AZ 1 CIDR"
   default = "192.168.10.0/24"
 }
 
-variable "private_subnet_2_CIDR" {
-  description = "Private Subnet AZ 1 CIDR"
-  default = "192.168.20.0/24"
+variable "ec2_type" {
+  description = "The type of ec2 instances to create"
+  default = "t2.micro"
+}
+
+variable "ec2_ami" {
+  description = "The ami image to use for ec2 instances (Ubuntu 16.04)"
+  default = "ami-0e4edb3648cd12c6b"
+}
+
+variable "access_key" {
+  type        = string
+  default     = ""
+}
+
+variable "secret_key" {
+  type        = string
+  default     = ""
 }
 
 variable "public_key_name" {
